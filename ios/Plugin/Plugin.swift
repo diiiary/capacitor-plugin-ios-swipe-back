16bdfee860816678b50382a899c950ef076252cb
Mesbah Mowlavi <mesbahm@mmoser.com>
2020-11-10 18:57:38 -0800

    Dispatch UI calls on main thread.

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -9,16 +9,20 @@ import Capacitor
 public class IosSwipeBack: CAPPlugin {
     
     @objc func enable(_ call: CAPPluginCall) {
-        self.bridge.getWebView()?.allowsBackForwardNavigationGestures = true;
-        call.resolve([
-            "error": 0
-            ]);
+        DispatchQueue.main.async {
+            self.bridge.getWebView()?.allowsBackForwardNavigationGestures = true;
+            call.resolve([
+                "error": 0
+                ]);
+        }
     }
     
     @objc func disable(_ call: CAPPluginCall) {
-        self.bridge.getWebView()?.allowsBackForwardNavigationGestures = false;
-        call.resolve([
-            "error": 0
-            ]);
+        DispatchQueue.main.async {
+            self.bridge.getWebView()?.allowsBackForwardNavigationGestures = false;
+            call.resolve([
+                "error": 0
+                ]);
+        }
     }
 }
